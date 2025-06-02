2eb73c1b80cad04a6d433f44084e85f127e42e37
Dharmik Zanzmera <dharmikzanzmera8@gmail.com>
2025-06-02 11:25:18 +0530

    Add files via upload

---
+++ njs program/app.js
@@ -0,0 +1,7 @@
+module.exports={
+    a:10,
+    b:30,
+    c:function(){
+        return 18;
+    }
+}
---
+++ njs program/index.js
@@ -0,0 +1,24 @@
+// const app = require('./app')
+
+// console.log("dhamo")
+// console.log(app.c())
+
+// const arr = [1,5,8,3,6,3,8,3,];
+
+// let result = arr.filter((item)=>{
+//     return item === 3;
+// })
+
+// console.log(result)
+
+//--------------------------------------------------------------------------------------------
+
+// const fs = require('fs')
+
+// fs.writeFileSync("raptile.txt","exportza")
+
+// console.log("raptile",__dirname)
+// console.log("raptile",__filename)
+
+//------------------------------------------------------------------------------------------------
+
